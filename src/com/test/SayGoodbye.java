ef9a75311861b83a2403835550ab02221c7856ec
xinglong.xia <xinglong.xia@rakuten.com>
2016-01-13 17:45:10 +0800

    add a new line

--- src/com/test/SayGoodbye.java
+++ src/com/test/SayGoodbye.java
@@ -4,6 +4,8 @@ public class SayGoodbye {
 
     public static void main(String[] args) {
         System.out.println("Goodbye yesterday, and hello new life!");
+        
+        System.out.println("Life is beautiful, enjoy it!!");
     }
 
 }
